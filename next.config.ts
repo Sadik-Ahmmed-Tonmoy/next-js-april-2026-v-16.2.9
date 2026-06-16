892dcf9c6ea39a9b40c1e077e2ef3bf086457ab9
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2026-06-16 14:39:13 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "5000",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
